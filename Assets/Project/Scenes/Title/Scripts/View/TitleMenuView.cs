9df8e1af9a5db7b5222a2d337821485665251fa3
doguto <doguto110@gmail.com>
2025-07-06 18:22:17 +0900

    Feat: タイトル背景画面の更新処理の作成

--- Assets/Project/Scenes/Title/Scripts/View/TitleMenuView.cs
+++ Assets/Project/Scenes/Title/Scripts/View/TitleMenuView.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 using Project.Commons.Scripts.View.UI;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Scenes.Title.Scripts.View
 {
     public class TitleMenuView : MonoBehaviour
     {
         [SerializeField] ButtonList buttonList;  // 0: Start, 1: Option
+        [SerializeField] List<Sprite> backgroundSprites;
+        [SerializeField] SpriteRenderer backgroundRenderer;
 
         public IObservable<Unit> OnPressedStart => buttonList.GetButtonEvent(0);
         public IObservable<Unit> OnPressedExit => buttonList.GetButtonEvent(1);
@@ -24,7 +29,7 @@ namespace Project.Scenes.Title.Scripts.View
 
         public void SetBackGround(int clearedStageAmount)
         {
-            
+            backgroundRenderer.sprite = backgroundSprites[clearedStageAmount];
         }
     }
 }
