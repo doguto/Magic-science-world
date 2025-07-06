9515abf02a6032a2af9579c9ac1ea4479951e123
Nodoka <131944632+purple-comet@users.noreply.github.com>
2025-07-06 21:19:26 +0900

    シナリオシーンを追加 (#25)
    * add: キャラ絵とnotoSerif日本語フォントのインストール
    
    * add: scene changes
    
    * test: ScriptableObjectのサンプルを作る
    
    * feat: 会話データのJSONをasset化、テキスト送りの機能追加（コンパイルエラーアリ）
    
    * chore: renameとScenarioViewのmodel参照防止
    
    * refactor: renameとコード整理
    
    * feat: 画像送り処理の実装
    
    * chore: update Unity change
    
    * fix; JsonImportを治す、立ち絵の振り分け
    
    * refactor: テストコードを削除

---
+++ Assets/Project/Commons/Scripts/Constants/MainCharacter.cs
@@ -0,0 +1,9 @@
+namespace Project.Commons.Scripts.Constants
+{
+  public static class MainCharacter
+  {
+    public static readonly string Me = "テン";
+    // いる？
+    // public static readonly string[] Enemy = ["ロコ", "アズマ", "タツミ", "ウシトラ", "スイ", "ハナレ", "コン"];
+  }
+}
