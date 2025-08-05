43de6b15d1015c07258326294bdfa8bf0f260fdd
doguto <doguto110@gmail.com>
2025-08-05 21:38:44 +0900

    Temp: ScrollableButtonListの作成著中

--- Assets/Project/Commons/Scripts/View/UI/ScrollableButtonList.cs
+++ Assets/Project/Commons/Scripts/View/UI/ScrollableButtonList.cs
@@ -1,12 +1,21 @@
+using UnityEngine;
+
 namespace Project.Commons.Scripts.View.UI
 {
     public class ScrollableButtonList : ButtonListBase
     {
+        [SerializeField] float buttonInterval;
+        
         public override void Init(ButtonListType buttonListType, int index = 0, bool isActive = false)
         {
+            this.buttonListType = buttonListType;
             
+            SetButtonIndex(index);
+            buttons[ButtonIndex].SetActive(true);
+            
+            SetActive(isActive);
         }
-
+        
         public override void MoveNext(bool isUp = true)
         {
             
