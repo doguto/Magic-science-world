78e5f49a4ed0226279396d28d5659325e26de99e
doguto <doguto110@gmail.com>
2025-09-28 16:58:45 +0900

    Feat: 一旦Charaの画像をTextureに移動

--- Assets/Project/Scripts/Repository/GameDatabase.cs
+++ Assets/Project/Scripts/Repository/GameDatabase.cs
@@ -1,7 +1,15 @@
+using Project.Scripts.Model;
+
 namespace Project.Scripts.Repository
 {
     public class GameDatabase
     {
         public static GameDatabase Instance { get; private set; }
+
+        public T Find<T>(int id) where T : ModelBase
+        {
+            T t;
+            return t;
+        }
     }
 }
