305143d12419fbd619781a3ba27a1e0bfe52bbac
purple-comet <131944632+purple-comet@users.noreply.github.com>
2025-09-28 22:54:33 +0900

    Remove: Constantsディレクトリとそのasmdefを削除

--- Assets/Project/Commons/Scripts/Constants/MainCharacter.cs
+++
@@ -1,9 +0,0 @@
-namespace Project.Commons.Scripts.Constants
-{
-  public static class MainCharacter
-  {
-    public static readonly string Me = "テン";
-    // いる？
-    // public static readonly string[] Enemy = ["ロコ", "アズマ", "タツミ", "ウシトラ", "スイ", "ハナレ", "コン"];
-  }
-}
