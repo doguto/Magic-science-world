600c015226b4f71b0d766a1bc7c71ba16f0dc5de
doguto <doguto110@gmail.com>
2025-09-28 21:49:56 +0900

    Refactor: ModelListTableの変数名を変更

--- Assets/Project/Scripts/Repository/ModelListTable.cs
+++ Assets/Project/Scripts/Repository/ModelListTable.cs
@@ -5,19 +5,19 @@ namespace Project.Scripts.Repository
 {
     public class ModelListTable<T> where T : ModelBase, new()
     {
-        readonly List<T> modelList = new();
+        readonly List<T> models = new();
 
         public T FindByIndex(int index)
         {
             if (index < 0) return null;
-            if (index >= modelList.Count) return null;
+            if (index >= models.Count) return null;
 
-            return modelList[index];
+            return models[index];
         }
 
         public List<T> All()
         {
-            return modelList;
+            return models;
         }
     }
 }
