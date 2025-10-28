86782522654537bf69e7f262874ddb279a902d58
doguto <doguto110@gmail.com>
2025-10-28 23:25:10 +0900

    Feat: GlobalSceneの基本骨子の作成

---
+++ Assets/Project/Scenes/Global/Scripts/Presenter/GlobalScenePresenter.cs
@@ -0,0 +1,11 @@
+﻿using Project.Scenes.Global.Scripts.View;
+using Project.Scripts.Presenter;
+using UnityEngine;
+
+namespace Project.Scenes.Global.Scripts.Presenter
+{
+    public class GlobalScenePresenter : MonoPresenter
+    {
+        [SerializeField] SoundManagerView soundManagerView;
+    }
+}
---
+++ Assets/Project/Scenes/Global/Scripts/View/SoundManagerView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Project.Scenes.Global.Scripts.View
+{
+    public class SoundManagerView : MonoBehaviour
+    {
+        [SerializeField] AudioSource audioSource;
+    }
+}
