9a692b7ae5a5ee702be646232b7140703d5089d3
doguto <doguto110@gmail.com>
2025-11-13 14:32:06 +0900

    Refactor: Get()の引数をstageIdに変更

--- Assets/Project/Scripts/Repository/ModelRepository/StageModelRepository.cs
+++ Assets/Project/Scripts/Repository/ModelRepository/StageModelRepository.cs
@@ -9,7 +9,7 @@ namespace Project.Scripts.Repository.ModelRepository
     public class StageModelRepository : ModelRepositoryBase
     {
         public static StageModelRepository Instance { get; } = new();
-        
+
         readonly List<StageData> stageData;
         readonly List<StageModel> stageModels = new();
 
@@ -23,15 +23,27 @@ namespace Project.Scripts.Repository.ModelRepository
             }
         }
 
-        public StageModel Get(int index)
+        public StageModel Get(int stageId)
         {
-            if (stageModels.Count <= index) return null;
+            var index = stageId - 1;
+            if (index < 0 || stageModels.Count <= index) return null;
+            if (stageModels.Count <= index) return stageModels[index];
+
+            for (var i = stageModels.Count; i < stageData.Count; i++)
+            {
+                stageModels.Add(new StageModel(stageData[i]));
+            }
 
             return stageModels[index];
         }
 
         public List<StageModel> All() => stageModels;
 
+        public void Clear()
+        {
+            stageModels.Clear();
+        }
+        
         List<StageData> LoadData()
         {
             var dataObject = Addressables.LoadAssetAsync<StageDataObject>(DataAddress).WaitForCompletion();
