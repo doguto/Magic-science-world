b4ab2ebb864e93b0b19293ce9772ac8e6f40b439
PREtz0302 <ryuna0302@gmail.com>
2025-11-13 21:08:04 +0900

    chore: 変数名のtypo修正

--- Assets/Project/Scripts/Model/StageModel.cs
+++ Assets/Project/Scripts/Model/StageModel.cs
@@ -11,14 +11,14 @@ namespace Project.Scripts.Model
         public StageData StageData { get; }
 
         public Sprite CharaImage { get; }
-        public bool IsOpend { get; private set; }
+        public bool IsOpened { get; private set; }
         public bool IsCleared { get; private set; }
 
 
         public StageModel(StageData stageData, bool isOpened = false, bool isCleared = false)
         {
             StageData = stageData;
-            IsOpend = isOpened;
+            IsOpened = isOpened;
             IsCleared = isCleared && isOpened; // 念のため isOpened と AND する
 
             var stillAssetRepository = new StillAssetRepository();
@@ -27,12 +27,12 @@ namespace Project.Scripts.Model
 
         public void Open()
         {
-            IsOpend = true;
+            IsOpened = true;
         }
 
         public void Clear()
         {
-            if (!IsOpend) throw new Exception("ステージが開放されていません.");
+            if (!IsOpened) throw new Exception("ステージが開放されていません.");
 
             IsCleared = true;
         }
